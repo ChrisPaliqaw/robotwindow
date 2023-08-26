7f18884bec7bd95552cf69bb1a342c30197ede64
Chris Paliqaw <chris@qalang.com>
2023-08-26 23:35:50 +0800

    refactor: use drei OrbitControls

--- web/src/components/World/World.tsx
+++ web/src/components/World/World.tsx
@@ -1,20 +1,13 @@
-import { useRef } from 'react'
-
-import { extend, useThree } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls } from '@react-three/drei'
 
 import Map from '../Map/Map'
 import Robot from '../Robot/Robot'
 import Transform from '../Transform/Transform'
 
-extend({ OrbitControls })
-
 const World = () => {
-  const { camera, gl } = useThree()
-
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      <OrbitControls />
       <directionalLight position={[1, 2, 3]} intensity={1.5} />
       <ambientLight intensity={0.25} />
       <Robot />
