ca5c01279941e978dbe0f08c8072b5d86cc57be1
Chris Paliqaw <chris@qalang.com>
2023-08-28 23:44:58 +0800

    feature: V (for Viam) decal on the top of the robot to indicate   orientation

--- web/src/components/Robot/Robot.tsx
+++ web/src/components/Robot/Robot.tsx
@@ -1,19 +1,26 @@
+import { Image } from '@react-three/drei'
 import * as THREE from 'three'
 
 import Transform from '../Transform/Transform'
 
 function Robot({ position }) {
-  console.log(position)
+  // Dimensions: L 32 cm x W 22 cm x H 7.5 cm
+  const length = 0.32
+  const width = 0.22
+  const height = 0.075
+  const slightlyAboveSurface = 0.51
   return (
-    // Dimensions: L 32 cm x W 22 cm x H 7.5 cm
-    <mesh
-      position-x={position.x}
-      position-y={position.z}
-      position-z={position.y}
-    >
-      <boxGeometry args={[0.32, 0.075, 0.22]} />
+    <mesh position={[position.x, position.z, position.y]}>
+      <boxGeometry args={[length, height, width]} />
       <meshStandardMaterial color="dimgray" wireframe={false} />
       <Transform />
+      <Image
+        url="viam.png"
+        rotation-x={Math.PI / -2}
+        scale={0.1}
+        // 0.51 is slight above surface to make sure it's visible
+        position-y={height * slightlyAboveSurface}
+      />
     </mesh>
   )
 }
